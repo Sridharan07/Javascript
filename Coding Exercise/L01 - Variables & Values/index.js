fe2fe8d17bd64dee08c844555b1593f8aee9819f
Sridharan07 <sridharansri98@gmail.com>
2026-06-16 19:20:02 -0500

    L01 - Variable & Values coding exercise

---
+++ Coding Exercise/L01 - Variables & Values/index.js
@@ -0,0 +1,47 @@
+// Declare and Initialize a variable
+let x=10;
+console.log(x);
+
+// Declare a variable
+let y;
+console.log(y);
+
+// Initial a variable
+y = 20;
+console.log(y);
+
+// Name Convertions
+let userName = "SriMu"; // Correct Method use camelcase convertion
+let first_name = 'Amirta'; // Underscore is  not recommended
+
+console.log('Hi ', userName);
+console.log('Hi ', first_name);
+
+// Incorrect Variable Name
+//let 5date = 5; // Invalid: A variable name cannot start with a number.
+//let john^jane = 'friend'; // Invalid: Variable names cannot contain special characters.
+
+// Reserved Keywords (Cannot Use)
+// let new = 'data'; Invalide = Because New is reserved keyword
+
+//Only $ symbol is valid in variable, that too it need to begin with $
+
+//Const
+const pi = 3.14;
+console.log(pi); // Const variable is used for fixed value/constant value. Const should be declare and initialize iin the same line
+
+//Var
+var job = 'Product Designer';
+console.log(job);
+
+//Scope of variable will on functions and loops
+//var = function scoped
+//let = block scoped
+//const = block scoped
+
+// Var = Redeclaration is allowed
+//let = Redeclaration is not allowed but can be reassignment is allowed
+//const = Both redeclaration and reassignment are not allowed
+
+
+
