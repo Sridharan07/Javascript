4a5d37b02f6a5ce2401557fe83e654bcfc8bf82e
Sridharan07 <sridharansri98@gmail.com>
2026-09-22 10:45:19 -0500

    DOM - getElementById

--- Coding Exercise/L12 - DOM Selecting & Modifying Element/index.js
+++ Coding Exercise/L12 - DOM Selecting & Modifying Element/index.js
@@ -7,3 +7,22 @@ The following the way to Selecting & Modifying Element in DOM
 4.querySelector
 5.querySelectorAll
 */
+
+//1.getElementById
+
+const heading = document.getElementById('main-heading');
+console.log(heading);
+
+console.log(heading.innerHTML); // Outputs the HTML content inside the element
+console.log(heading.textContent); // Outputs the Content inside the element.
+
+//Chaning the value
+heading.innerHTML = 'See, Im from the planet Earth';
+
+function changeHeading(){
+    setTimeout(() =>{
+        heading.textContent = 'See, Im from the planet Mars';
+    }, 4000);
+}
+changeHeading();
+
